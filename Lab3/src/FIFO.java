ae51b86e43f409462648fa9b3454f8366b127647
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-02 16:10:20 +0100

    Initial FIFO queue working

--- Lab3/src/FIFO.java
+++ Lab3/src/FIFO.java
@@ -1,46 +1,98 @@
+import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-public class FIFO implements Queue{
+public class FIFO implements Queue {
+
+	private ArrayList<Object> fifo;
+
+	public FIFO() {
+		this.fifo = new ArrayList<>();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FIFO f = new FIFO();
+		f.add(1);
+		f.add(2);
+		f.add(3);
 
+		System.out.println(f.first());
+		f.removeFirst();
+		System.out.println(f.first());
+		f.removeFirst();
+		System.out.println(f.first());
 	}
 
 	@Override
 	public void add(Object arg0) {
-		// TODO Auto-generated method stub
-		
+		fifo.add(arg0);
 	}
 
 	@Override
 	public Object first() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		if (fifo.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		return fifo.get(0);
 	}
 
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		return false;
+		return fifo.size() == 0 ? true : false;
 	}
 
 	@Override
 	public int maxSize() {
 		// TODO Auto-generated method stub
-		return 0;
+		return 1000;
 	}
 
 	@Override
 	public void removeFirst() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		
+		if (fifo.size() == 0) {
+			throw new NoSuchElementException();
+		}
+		fifo.remove(0);
 	}
 
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
-		return 0;
+		return fifo.size();
+	}
+
+	public Object get(int i) {
+		return this.fifo.get(i);
+	}
+
+	/**
+	 * Two objects are equal if and only if they are both instances of {@link FIFO},
+	 * and have the same elements in order.
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this.fifo.size() != ((FIFO) o).size()) {
+			return false;
+		}
+
+		for (int i = 0; i < this.fifo.size(); i++) {
+			if (this.fifo.get(i) != ((FIFO) o).get(i)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		String result = "";
+
+		for (Object o : this.fifo) {
+			result += o.toString();
+		}
+
+		return result;
 	}
 
 }
