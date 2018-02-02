f2d501f31b19c7e6e53d4f2b664d47ae1453d8ff
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-02 15:36:52 +0100

    Initial creation

---
+++ Lab3/src/FIFO.java
@@ -0,0 +1,46 @@
+import java.util.NoSuchElementException;
+
+public class FIFO implements Queue{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(Object arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object first() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int maxSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void removeFirst() throws NoSuchElementException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
