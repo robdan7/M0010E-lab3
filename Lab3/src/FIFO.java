c35461445e44215287da9a5931bc9f7c8fc82336
Robin <Robin@DESKTOP-1C44T3I.lan>
2018-02-03 08:15:57 +0100

    Javadoc

--- Lab3/src/FIFO.java
+++ Lab3/src/FIFO.java
@@ -8,6 +8,7 @@ import java.util.NoSuchElementException;
  */
 
 public class FIFO implements Queue {
+	private int maxSize;
 
 	private ArrayList<Object> fifo;
 
@@ -16,18 +17,28 @@ public class FIFO implements Queue {
 	}
 
 	public static void main(String[] args) {
-		String[] file = {"graph.txt"};
-		
+		String[] file = { "graph.txt" };
+
 		FIFOmain.main(file);
-		
+
 		BFTmain.main(file);
 	}
 
+	/**
+	 * Add an object at the end of the queue.
+	 */
 	@Override
 	public void add(Object arg0) {
 		fifo.add(arg0);
+		this.maxSize = this.fifo.size() > this.maxSize ? this.fifo.size() : this.maxSize;
 	}
 
+	/**
+	 * This method returns the first element in the queue.
+	 * 
+	 * @throws NoSuchElementException
+	 *             This is thrown if the queue is empty.
+	 */
 	@Override
 	public Object first() throws NoSuchElementException {
 		if (fifo.size() == 0) {
@@ -36,16 +47,24 @@ public class FIFO implements Queue {
 		return fifo.get(0);
 	}
 
+	/**
+	 * <p>
+	 * This method returns <b>true</b> if, and only if, the size of this queue is 0.
+	 * Otherwise, <b>false</b> is returned.
+	 * </p>
+	 */
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return fifo.size() == 0 ? true : false;
 	}
 
+	/**
+	 * @return The maximum size this queue has had since it was created.
+	 */
 	@Override
 	public int maxSize() {
-		// TODO Auto-generated method stub
-		return 1000;
+		return this.maxSize;
 	}
 
 	@Override
@@ -58,7 +77,6 @@ public class FIFO implements Queue {
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
 		return fifo.size();
 	}
 
@@ -69,6 +87,9 @@ public class FIFO implements Queue {
 	/**
 	 * Two objects are equal if and only if they are both instances of {@link FIFO}
 	 * and have the same elements in order.
+	 * 
+	 * @throws ClassCastException
+	 *             This is thrown is o is not an instance of FIFO.
 	 */
 	@Override
 	public boolean equals(Object o) throws ClassCastException {
@@ -85,8 +106,8 @@ public class FIFO implements Queue {
 
 		// o is a queue with the same size. Check if the objects are equal.
 		loop: for (int i = 0; i < this.fifo.size(); i++) {
-			if (this.fifo.get(i) == null || ((FIFO)o).get(i) == null) {
-				if (this.fifo.get(i) != ((FIFO)o).get(i)) {
+			if (this.fifo.get(i) == null || ((FIFO) o).get(i) == null) {
+				if (this.fifo.get(i) != ((FIFO) o).get(i)) {
 					result = false;
 					break loop;
 				}
@@ -99,6 +120,14 @@ public class FIFO implements Queue {
 		return result;
 	}
 
+	/**
+	 * <p>
+	 * This method returns a string beginning with <b>"Queue: "</b> followed by the
+	 * following, for each element in the queue.
+	 * </p>
+	 * 
+	 * @return A string.
+	 */
 	@Override
 	public String toString() {
 		String result = "Queue: ";
@@ -106,7 +135,7 @@ public class FIFO implements Queue {
 		if (this.fifo.size() == 0) {
 			return result;
 		}
-		
+
 		for (Object o : this.fifo) {
 			result += "(" + String.valueOf(o) + ") ";
 		}
